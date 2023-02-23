1853e31ff8532c5fc64867aa30e8e321b7dfa603
IgorFilin <filinigor@yandex.ru>
2023-02-23 12:06:55 +0300

    add c

--- src/store/store.js
+++ src/store/store.js
@@ -1,24 +1,25 @@
-import { createApp } from 'vue'
-import { createStore } from 'vuex'
-import App from './App.vue'
+import { createApp } from "vue";
+import { createStore } from "vuex";
+import App from "./App.vue";
 
-const b =  [1,2,3,4,5]
-const a = 0
+const b = [1, 2, 3, 4, 5];
+const a = 0;
+const c = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
 export const store = createStore({
-  state () {
+  state() {
     return {
-      count: 0
-    }
+      count: 0,
+    };
   },
   mutations: {
-    increment (state) {
-      state.count++
-    }
-  }
-})
+    increment(state) {
+      state.count++;
+    },
+  },
+});
 
-const app = createApp(App)
+const app = createApp(App);
 
 // Install the store instance as a plugin
-app.use(store)
+app.use(store);
